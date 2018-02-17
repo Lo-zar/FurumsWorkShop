5fb7b9a3db08a94bb5ac54c33d50f2976da4dcca
sizer <sizer2189@gmail.com>
2018-02-17 16:34:51 +0200

    add some twacks

--- ATM.py
+++ ATM.py
@@ -1,22 +1,20 @@
 def withdrawl(Balance, Request):
-	allowed_papers = [100, 50, 20, 10, 5, 2, 1]
-	if Request > Balance:
-		print("You Don,t Have All This Money: You Only Have "+ str (Balance))
-	elif Request <= 0:
-		print("Requested Balance Must Be More Than 0")
-	else:
-		print("Current Balance = "+ str (Balance))
-		print("Requested = ", Request)
-		Balance = Balance - Request
-		for i in allowed_papers:
-			while Request >= i:
-				Request -= i
-				print("Give:", i)
-		print("Remainder Balance "+ str (Balance))
-		return Balance
-
+    result = Balance
+    allowed_papers = [100, 50, 20, 10, 5, 2, 1]
+    sizer = "_-___----____ ATM MACHINE ____----___-_"
+    print(sizer)
+    if Request <= Balance:
+        for i in allowed_papers:
+            while Request >= i:
+                print("Give:", i)
+                Request -= i
+                Balance -= i
+        print("Remainder Balance " + str(Balance))
+        return Balance
+    elif Request >= Balance:
+        print("You Don,t Have All This Money, Your Balance Is " + str(Balance))
+    print(sizer)
 My_Balance = 577
-My_Balance = withdrawl(My_Balance,277)
-My_Balance = withdrawl(My_Balance,250)
-My_Balance = withdrawl(My_Balance,250)
-
+My_Balance = withdrawl(My_Balance, 277)
+My_Balance = withdrawl(My_Balance, 250)
+My_Balance = withdrawl(My_Balance, 250)
