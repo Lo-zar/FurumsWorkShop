1481bbd25fb184d87be32a6b5977788b07fe3ac7
sizer <sizer2189@gmail.com>
2018-02-18 15:52:41 +0200

    ورشة عمل تطبيقية لمسار الويب الشامل: مشاركة حلول Lists

--- ATM.py
+++ ATM.py
@@ -7,18 +7,20 @@ class ATM:
         self.Withdrawals_List = []
 
     def Withdraw(self, Request):
-        print("WELCOME TO", self.Bank_Name)
-        print("Available Balance =", self.Balance)
-        self.seperator()
+        print("WELCOME TO {0}".format(self.Bank_Name))
+        print("Your Balance Is: {0}$ And You Requested {1}$.".format(
+            self.Balance, Request))
 
-        if Request > self.Balance:
+        if self.Balance <= 0:
+            print("Insufficient Balance, Please Deposit Money And ,Try Agein @_@")
+
+        elif Request > self.Balance:
             print("I can,t Give You All This Money @_@ , Your Balance Is " + str(self.Balance))
-        elif Request <= 0:
-            print("Same Thing Was False? ,Try Agein @_@")
+
         else:
             self.Withdrawals_List.append(Request)
             self.Balance -= Request
-            Bills = [100, 50, 10, 5, 2, 1]
+            Bills = [100, 50, 10, 5]
             for Currency in Bills:
                 Bills_number = int(Request / Currency)
                 Request -= Currency * Bills_number
@@ -34,9 +36,10 @@ class ATM:
         print("_-___----____ ATM MACHINE ____----___-_")
         previous_Balance = self.Balance + sum(self.Withdrawals_List)
         print("Previous Balance:", previous_Balance)
-
+        index =1
         for Withdrawal in self.Withdrawals_List:
-            print("withdrawal:", Withdrawal)
+            print("{0} Withdrawal No.{1}: {2}$".format(self.Bank_Name, index, Withdrawal))
+            index += 1
 
         print("All withdrawals:", sum(self.Withdrawals_List))
         print("Available Balance:", previous_Balance - sum(self.Withdrawals_List))
@@ -52,7 +55,7 @@ Balance2 = 1000
 atm1 = ATM(Balance1, "BNP BANK")
 atm2 = ATM(Balance2, "WATAN BANK")
 
-atm1.Withdraw(300)
+atm1.Withdraw(500)
 atm1.Withdraw(250)
 
 atm2.Withdraw(200)
