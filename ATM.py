4c8a989b3b6547ce6747b00c1b66973d8b4b2182
sizer <sizer2189@gmail.com>
2018-02-18 23:48:18 +0200

    واخيرا

--- ATM.py
+++ ATM.py
@@ -1,12 +1,13 @@
 class ATM:
-    print("_-___----____ ATM MACHINE ____----___-_")
+    LOGO = "^^^^^^^^^^^^***$$$ ATM MACHINE $$$***^^^^^^^^^^^^"
 
     def __init__(self, balance, bank_name):
         self.balance = balance
         self.bank_name = bank_name
         self.withdrawals_list = []
 
-    def Withdraw(self, request):
+    def withdrawal(self, request):
+        print(ATM.LOGO)
         print("WELCOME TO {0}".format(self.bank_name))
         print("Your Balance Is: {0}$ And You Requested {1}$.".format(
             self.balance, request))
@@ -17,46 +18,44 @@ class ATM:
         elif request > self.balance:
             print("I can,t Give You All This Money @_@ , Your Balance Is " + str(self.balance))
 
-    def withdrawals_list:
-        self.withdrawals_list.append(request)
-        self.balance -= request
-        bills = [100, 50, 10, 5]
-        for currency in bills:
-            bills_number = int(request / currency)
-            request -= currency * bills_number
-            for i in range(Bills_number):
-                print("give", currency)
+        elif request <= 0:
+                print("Try Agine")
+
+        else:
+            self.withdrawals_list.append(request)
+            self.balance -= request
+            bills = [100, 50, 10, 5, 2, 1]
+            for currency in bills:
+                bills_number = int(request / currency)
+                request -= currency * bills_number
+                for i in range(bills_number):
+                    print("give", currency)
             if request > 0:
                 print("give", request)
-
-        self.seperator()
+        print(ATM.LOGO)
         return self.balance
 
     def show_withdrawals(self):
-        print("_-___----____ ATM MACHINE ____----___-_")
-        previous_balance = self.balance + sum(self.withdrawals_list)
-        print("previous balance:", previous_balance)
-        index =1
-        for Withdrawal in self.Withdrawals_List:
-            print("{0} withdrawal No.{1}: {2}$".format(self.bank_name, index, withdrawal))
+        index = 1
+        for Withdrawal in self.withdrawals_list:
+            print("{0} withdrawal No.{1}: {2}$".format(self.bank_name, index, Withdrawal))
             index += 1
-
         print("all withdrawals:", sum(self.withdrawals_list))
-        print("available balance:", previous_balance - sum(self.withdrawals_list))
-        self.seperator()
-
-    def seperator(self):
-    	print("_-___----____ ATM MACHINE ____----___-_")
-
-
+        print("available balance:", self.balance)
+        print(ATM.LOGO)
 balance1 = 500
 balance2 = 1000
 
-atm1 = ATM(balance1, "BNP BANK")
-atm2 = ATM(balance2, "WATAN BANK")
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+atm1.withdrawal(277)
+atm1.withdrawal(52)
+atm1.withdrawal(1000)
+atm1.withdrawal(-5)
 
-atm1.withdraw(500)
-atm1.withdraw(250)
+atm2.withdrawal(654)
+atm2.withdrawal(106)
+atm2.withdrawal(900)
 
-atm2.withdraw(200)
-atm2.withdraw(100)
+atm1.show_withdrawals()
+atm2.show_withdrawals()
